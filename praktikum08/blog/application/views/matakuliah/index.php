d6974fe2461dfff65004db59f3145e0e92afcc41
Tsabbitaa <91918043+Tsabbitaa@users.noreply.github.com>
2022-05-14 12:54:27 +0700

    Add files via upload

---
+++ praktikum08/blog/application/views/matakuliah/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matakuliah</title>
+</head>
+<body>
+    <h3>Matakuliah</h3>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>SKS</th>
+                <th>Kode</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $nomor = 1;
+            foreach ($matakuliah as $mtkl) {
+            ?>
+            <tr>
+                <td><?php echo $nomor?></td>
+                <td><?php echo $mtkl -> nama?></td>
+                <td><?php echo $mtkl -> sks?></td>
+                <td><?php echo $mtkl -> kode?></td>
+            </tr>
+            <?php
+            $nomor++;
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
